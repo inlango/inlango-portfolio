2a33ff2abcded34218d62fbfb6f7175a83a0777b
utsav <uts.lama@gmail.com>
2020-05-21 21:34:22 +1000

    version 2 - mobile respnsive, homepage components

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@
 //  const element = <h1></h1>;
 
 //ReactDOM.render(element, document.getElementById("app"));
+
+
+const toggleMenu = () => {
+
+    var x = document.getElementById("inlango-responsive-menu");
+    if (x.className === "inlango-responsive") {
+        x.className = " ";
+    } else {
+        x.className = "inlango-responsive";
+    }
+}
+
+
+document.getElementById("inlango-menu-button").addEventListener("click", toggleMenu);
