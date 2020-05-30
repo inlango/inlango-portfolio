72d715259eaa517597fd2beff207968bf2b32ee3
utsav <uts.lama@gmail.com>
2020-05-30 16:05:15 +1000

    fixed spelling

--- src/services.js
+++ src/services.js
@@ -13,7 +13,7 @@ export default class Services extends React.Component {
 
                 {/*<!--white container-->*/}
                 <div className="inlango-container inlango-container-light" id="services-content">
-                    <h1>Mouse over to servies to see details</h1>
+                    <h1>click to see details</h1>
                     <Tabs />
                 </div>
             </div>
