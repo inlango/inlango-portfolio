0f33fc699920dfc7b4c5b9388b82bf37b19b8980
utsav <uts.lama@gmail.com>
2020-08-08 13:38:14 +1000

    added backend server and sending email for contact form from nodemailer

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,77 @@
 const express = require("express");
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const cors = require("cors");
 
-require("dotenv").config();
+//require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
-
-app.get('/', (req, res)=> {
-    res.send('Hello');
-});
 
 //middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.json());
 
-app.listen(5000, () => {
-    console.log(`server is running on port" ${port}`);
+app.get('/', (req, res) => {
+    res.send('welcome to my form')
+});
+
+app.post('/api/form', (req, res) => {
+
+    let data = req.body
+    let smtpTransport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: "inlangoemail@gmail.com",
+            pass: "9841515319@Qwe"
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    let mailOptions = {
+        from: data.email,
+        to: 'admin@inlango.com',
+        subject: `Message from ${data.firstName}`,
+        html: `
+        <h3>Informations</h3>
+        <ul>
+            <li>Name: ${data.firstName}</li>
+            <li>Lastname: ${data.lastName}</li>
+            <li>Email: ${data.email}</li>
+        </ul>
+    
+        <h3>Message</h3>
+        <p>${data.message}</p>
+        
+        `
+    }
+    smtpTransport.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Server is ready to take our messages");
+        }
+    });
+    smtpTransport.sendMail(mailOptions, (error, response) => {
+
+        if (error) {
+            res.send(error);
+        }
+        else {
+            res.send('Success');
+            console.log('success');
+        }
+
+    })
+
+    smtpTransport.close();
+})
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
 });
